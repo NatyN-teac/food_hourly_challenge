45661d0861094e5f1585251c43e96e659faeae41
Naty <naty2work@gmail.com>
2020-05-08 14:49:42 +0300

    project setup done

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hourly Challenge',
+      theme: ThemeData(
+
+      ),
+      home: FoodHomePage(),
+    );
+  }
+}
+
+
+class FoodHomePage extends StatefulWidget {
+  @override
+  _FoodHomePageState createState() => _FoodHomePageState();
+}
+
+class _FoodHomePageState extends State<FoodHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
